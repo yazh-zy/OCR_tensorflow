12d37013a381303e3266d9a095e2b0c2d0987ae7
yazh <yangzhou.zy@outlook.com>
2018-06-15 14:17:12 +0100

    this version is trained with imagenet and it still has some bugs need to be solved, see the beginning of text.py

--- Mask_RCNN/text/text.py
+++ Mask_RCNN/text/text.py
@@ -9,6 +9,18 @@ Written by Waleed Abdulla
 ------------------------------------------------------------
 """
 
+'''
+python ./path/to/text.py train --dateset="./path/to/dataset" --weights=coco or imagenet
+python ./path/to/text.py detect --weight=./path/to/.h5 --image=./path/to/image
+'''
+
+'''
+Now it is trained with the imagenet weights for the start it is now to check whether it is can generate the .h5 weights file
+and it is not sure why it cannot use "find_last()" to get the last training weights, which still need to be found ASP.
+When it is detected without the color splash, it will generate an odd file which shows nothing and it shows some warning,
+so this problem also need to be solved, and the output should be some divided images only with some texts.
+'''
+
 import os
 import sys
 import json
@@ -26,7 +38,7 @@ ROOT_DIR = os.getcwd()
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
-from mrcnn import model as modellib, utils
+from mrcnn import model as modellib, utils, visualize
 
 # Path to trained weights file
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
@@ -56,22 +68,33 @@ class TextConfig(Config):
     # GPU_COUNT = 8
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  
+    NUM_CLASSES = 1 + 1
 
-    # Skip detections with < 50% confidence
-    DETECTION_MIN_CONFIDENCE = 0.5
+    #IMAGE_MIN_DIM = 1080
+    #IMAGE_MAX_DIM = 1080
+
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+    TRAIN_ROIS_PRE_IMAGE = 50
+
+    VALIDATION_STEPS = 5
+
+    # Skip detections with < 30% confidence
+    DETECTION_MIN_CONFIDENCE = 0.3
 
 
 ############################################################
 #  Dataset
 ############################################################
 class TextDataset(utils.Dataset):
+    
     def load_text(self, dataset_dir, subset):
 
+        #Add class, only has one class
         self.add_class("text", 1, "text")
 
         dataset_dir = os.path.join(dataset_dir, subset)
-        annotations = json.load(open(os.path.join(dataset_dir, "COCO_Text.json")))
+        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
         annotations = list(annotations.values())
 
         annotations = [a for a in annotations if a['regions']]
@@ -134,10 +157,85 @@ def train(model):
     # COCO trained weights, we don't need to train too long. Also,
     # no need to train all layers, just the heads should do it.
     print("Training network heads")
+
+    # Image Augmentation
+    # Right/Left flip 50% of the time
+    augmentation = imgaug.augmenters.Fliplr(0.5)
+
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=40,
                 layers='heads')
+    
+    # Training - Stage 2
+    # Finetune layers from ResNet stage 4 and up
+    print("Fine tune Resnet stage 4 and up")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=120,
+                layers='4+',
+                augmentation=augmentation)
+
+    # Training - Stage 3
+    # Fine tune all layers
+    print("Fine tune all layers")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE / 10,
+                epochs=160,
+                layers='all',
+                augmentation=augmentation)
+    
+
+    # Save weights
+    # Typically not needed because callbacks save after every epoch
+    # Uncomment to save manually
+    model_path = os.path.join(ROOT_DIR, "mask_rcnn_texts.h5")
+    model.keras_model.save_weights(model_path)
+    
+
+
+############################################################
+#  Detect
+############################################################
+def detect_image(model, image_path=None):
+    assert image_path
+    #####################TODO###################################
+    
+    # Run model detection and generate the color splash effect
+    print("Running on {}".format(args.image))
+    # Read image
+    image = skimage.io.imread(args.image)
+    # Detect objects
+    result = model.detect([image], verbose=1)[0]
+
+    splash = color_splash(image, result['masks'])
+
+    file_name = "detection_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+    # visualize.display_instances(image, result['rois'], result['masks'], result['class_ids'])
+    # skimage.io.imsave(file_name, result['rois'].astype(np.uint8))
+    skimage.io.imsave(file_name, splash)
+    
+    print("Saved to ", file_name)
+####################???????#######################################
+
+def color_splash(image, mask):
+    """Apply color splash effect.
+    image: RGB image [height, width, 3]
+    mask: instance segmentation mask [height, width, instance count]
+
+    Returns result image.
+    """
+    # Make a grayscale copy of the image. The grayscale copy still
+    # has 3 RGB channels, though.
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    # Copy color pixels from the original color image where mask is set
+    if mask.shape[-1] > 0:
+        # We're treating all instances as one, so collapse the mask into one layer
+        mask = (np.sum(mask, -1, keepdims=True) >= 1)
+        splash = np.where(mask, image, gray).astype(np.uint8)
+    else:
+        splash = gray.astype(np.uint8)
+    return splash
 
 
 ############################################################
@@ -151,7 +249,7 @@ if __name__ == '__main__':
         description='Train Mask R-CNN to detect Text.')
     parser.add_argument("command",
                         metavar="<command>",
-                        help="'train' or 'splash'")
+                        help="'train' or 'detect'")
     parser.add_argument('--dataset', required=False,
                         metavar="/path/to/Text/dataset/",
                         help='Directory of the Text dataset')
@@ -162,6 +260,9 @@ if __name__ == '__main__':
                         default=DEFAULT_LOGS_DIR,
                         metavar="/path/to/logs/",
                         help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--image', required=False,
+                        metavar="path or URL to image",
+                        help='Image to apply the color splash effect on')
 
     args = parser.parse_args()
     
@@ -188,6 +289,7 @@ if __name__ == '__main__':
     else:
         model = modellib.MaskRCNN(mode="inference", config=config,
                                   model_dir=args.logs)
+        # model_path = model.find_last()
 
 
     # Select weights file to load
@@ -220,9 +322,8 @@ if __name__ == '__main__':
     # Train or evaluate
     if args.command == "train":
         train(model)
-    elif args.command == "splash":
-        detect_and_color_splash(model, image_path=args.image,
-                                video_path=args.video)
+    elif args.command == "detect":
+        detect_image(model, image_path=args.image)
     else:
         print("'{}' is not recognized. "
-              "Use 'train' or 'splash'".format(args.command))
+              "Use 'train' or 'detect'".format(args.command))
